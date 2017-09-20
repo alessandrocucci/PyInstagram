469f2aec918f42b97c0894c58be6279ec288092f
Alessando Cucci <alessandro.cucci@energee3.com>
2017-09-20 09:15:56 +0200

    Modifica init

--- pyinstagram/__init__.py
+++ pyinstagram/__init__.py
@@ -1,6 +1,11 @@
 from .base import *
 from .oauth import *
 
-__version__ = "0.1"
-__author__ = "Alessandro Cucci <alessandro.cucci@gmail.com>"
-__license__ = "MIT"
+__title__ = 'pyinstagram'
+__description__ = 'Instagram HTTP wrapper for Python Developers.'
+__version__ = '0.1.2'
+__author__ = 'Alessandro Cucci'
+__author_email__ = 'alessandro.cucci@gmail.com'
+__author_url__ = 'http://www.alessandrocucci.it/'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Alessandro Cucci'
