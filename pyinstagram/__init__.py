a0bb9648417967978cc8c0b8b2749b9f11476601
Alessando Cucci <alessandro.cucci@energee3.com>
2017-09-20 12:20:44 +0200

    Modifica init

--- pyinstagram/__init__.py
+++ pyinstagram/__init__.py
@@ -1,3 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+
+██████╗ ██╗   ██╗██╗███╗   ██╗███████╗████████╗ █████╗  ██████╗ ██████╗  █████╗ ███╗   ███╗
+██╔══██╗╚██╗ ██╔╝██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██╔════╝ ██╔══██╗██╔══██╗████╗ ████║
+██████╔╝ ╚████╔╝ ██║██╔██╗ ██║███████╗   ██║   ███████║██║  ███╗██████╔╝███████║██╔████╔██║
+██╔═══╝   ╚██╔╝  ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║   ██║██╔══██╗██╔══██║██║╚██╔╝██║
+██║        ██║   ██║██║ ╚████║███████║   ██║   ██║  ██║╚██████╔╝██║  ██║██║  ██║██║ ╚═╝ ██║
+╚═╝        ╚═╝   ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+
+
+PyInstagram Library
+~~~~~~~~~~~~~~~~~~~
+
+PyInstagram è una libreria Python per il download
+di dati Instagram in formato json o "pythonico"
+
+Esempi di utilizzo:
+
+ >>> from pyinstagram import InstagramJsonClient
+ >>> app = InstagramJsonClient()
+ >>> media = app.get_by_user("cern", count=1)
+
+ >>> media = app.get_by_hashtag("milanofashionweek")
+ >>> for m in media:
+ >>>   print(m['display_src'])
+
+"""
 from .base import *
 from .oauth import *
 
