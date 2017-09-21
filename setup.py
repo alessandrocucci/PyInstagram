58c654fe3baa379dd239b19d2dd0aafe0eb83146
Alessandro Cucci <alessandro.cucci@gmail.com>
2017-09-21 15:13:10 +0200

    aggiornata versione

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyInstagram',
-    version='0.1.2',
+    version='0.1.3',
     packages=['pyinstagram'],
     url='www.alessandrocucci.it',
     license='MIT',
