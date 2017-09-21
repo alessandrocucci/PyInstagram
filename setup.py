72f07b003cd93b002e93ded6c7f0b15f62503964
Alessandro Cucci <alessandro.cucci@gmail.com>
2017-09-21 14:47:34 +0200

    modifica setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='PyInstagram',
-    version='0.1',
-    packages=['pyinstagram', 'requests'],
+    version='0.1.2',
+    packages=['pyinstagram'],
     url='www.alessandrocucci.it',
     license='MIT',
     author='Alessandro Cucci',
