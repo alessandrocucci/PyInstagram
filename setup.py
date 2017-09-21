bf4a8f56b79705728bff33e4f0e72843e1d9fb04
Alessandro Cucci <alessandro.cucci@gmail.com>
2017-09-21 14:43:55 +0200

    Aggiunto setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyInstagram',
+    version='0.1',
+    packages=['pyinstagram', 'requests'],
+    url='www.alessandrocucci.it',
+    license='MIT',
+    author='Alessandro Cucci',
+    author_email='alessandro.cucci@gmail.com',
+    description='Instagram Scraping'
+)
